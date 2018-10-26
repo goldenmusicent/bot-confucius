a17b2329ccd84fac53c560459d85af45695d0504
goldenmusicent <44490423+goldenmusicent@users.noreply.github.com>
2018-10-26 10:13:32 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -128,7 +128,7 @@ client.on("message", async message => {
       // Détermine son rôle futur
       if (member.roles.has(recrue.id)){
         await message.channel.send(member + "  Bravo pour ta promotion en tant que **Membre - ⭐⭐**! Soit en digne!");$
-        await mention.sendMessage("test privé");
+        mention.sendMessage("test privé");
 
         // Supression de l'ancien statuts
         let role_old = member.guild.roles.find(role => role.name === "Recrue - ⭐");
