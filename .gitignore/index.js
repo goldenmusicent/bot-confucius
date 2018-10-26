eaa3c5c9fb7ff7e92e38d85623730fade4d85444
goldenmusicent <44490423+goldenmusicent@users.noreply.github.com>
2018-10-26 11:06:59 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -127,7 +127,7 @@ client.on("message", async message => {
       // Détermine son rôle futur
       if (member.roles.has(recrue.id)){
         await message.channel.send(member + "  Bravo pour ta promotion en tant que **Membre - ⭐⭐**! Soit en digne!");
-        message.member.createDM().then(channel => {
+        member.createDM().then(channel => {
           channel.send("Heya!");
         });
 
