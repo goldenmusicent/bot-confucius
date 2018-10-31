cf2afb33ef5837555e174404057b4933b578fd51
goldenmusicent <44490423+goldenmusicent@users.noreply.github.com>
2018-10-31 15:41:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -428,7 +428,7 @@ client.on("message", async message => {
   	message.channel.send(member + "  a hérité d'un blâme  :sweat_smile: ");
 
     // Affichage dans els log
-    server.channels.get('507201091012657186').send({embed: {
+    client.channels.get('507201091012657186').send({embed: {
     color: 3447003,
 
     fields: [{
